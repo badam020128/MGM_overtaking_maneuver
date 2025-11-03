6f47598a66a07c71da00c62e6419ea99166def27
Bene Ádám <badam020128@gmail.com>
2025-11-03 19:21:19 +0100

    hátsó robot sebessége nagyobb

--- overtaking_maneuver/simple_move.py
+++ overtaking_maneuver/simple_move.py
@@ -6,27 +6,47 @@ class simple_move(Node):
     def __init__(self):
         super().__init__('robot_controller')
         
+        # Paraméterek: alapértelmezett sebességek robot1 és robot2 számára
+        # robot2_speed alapértelmezésben nagyobb, így a hátul jövő robot gyorsabb lesz
+        # Make robot1 the faster one by default
+        self.declare_parameter('robot1_speed', 0.4)
+        self.declare_parameter('robot2_speed', 0.2)
+        self.declare_parameter('hz', 10.0)
+
+        self.robot1_speed = float(self.get_parameter('robot1_speed').value)
+        self.robot2_speed = float(self.get_parameter('robot2_speed').value)
+        hz = float(self.get_parameter('hz').value)
+
         # Publisher-ek létrehozása mindkét robot cmd_vel topic-jához
         self.publisher_robot1 = self.create_publisher(Twist, '/robot1/cmd_vel', 10)
         self.publisher_robot2 = self.create_publisher(Twist, '/robot2/cmd_vel', 10)
-        
-        # Időzítő létrehozása, ami 10 Hz-en (0.1 másodpercenként) futtatja a callback-et
-        self.timer = self.create_timer(0.1, self.timer_callback)
-        self.get_logger().info('Robot Controller elindult, mindkét robotot előre mozgatja.')
+
+        # Időzítő létrehozása a megadott frekvenciával
+        period = 1.0 / hz if hz > 0 else 0.1
+        self.timer = self.create_timer(period, self.timer_callback)
+        self.get_logger().info(f'Robot Controller elindult: robot1_speed={self.robot1_speed}, robot2_speed={self.robot2_speed}, hz={hz}')
 
     def timer_callback(self):
-        # Előre mozgást definiáló Twist üzenet
-        msg = Twist()
-        msg.linear.x = 0.2  # Előre mozgás sebessége (m/s)
-        msg.linear.y = 0.0
-        msg.linear.z = 0.0
-        msg.angular.x = 0.0
-        msg.angular.y = 0.0
-        msg.angular.z = 0.0
+        # Előre mozgást definiáló Twist üzenetek külön a két robothoz
+        msg1 = Twist()
+        msg1.linear.x = self.robot1_speed  # robot1 sebessége (m/s)
+        msg1.linear.y = 0.0
+        msg1.linear.z = 0.0
+        msg1.angular.x = 0.0
+        msg1.angular.y = 0.0
+        msg1.angular.z = 0.0
+
+        msg2 = Twist()
+        msg2.linear.x = self.robot2_speed  # robot2 (hátul jövő) sebessége (m/s)
+        msg2.linear.y = 0.0
+        msg2.linear.z = 0.0
+        msg2.angular.x = 0.0
+        msg2.angular.y = 0.0
+        msg2.angular.z = 0.0
 
         # Parancs publikálása mindkét robotnak
-        self.publisher_robot1.publish(msg)
-        self.publisher_robot2.publish(msg)
+        self.publisher_robot1.publish(msg1)
+        self.publisher_robot2.publish(msg2)
 
 def main(args=None):
     rclpy.init(args=args)
